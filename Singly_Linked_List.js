5b42b2a54c5c8cbae68920d283bcd6079bd9226a
dpatel0114 <1991dharapatel@gmail.com>
2019-10-10 16:26:36 -0400

    First

---
+++ Singly_Linked_List.js
@@ -0,0 +1,46 @@
+// ? DATA STURCTURE : 1. SinglyLinked List: 
+// To make Node class 
+// TO make Singly Linked List class
+// TODO: First PUSH method ; 
+// TODO: Second POP method ;
+
+
+
+class Node{
+    constructor(val){
+      this.val = val;
+      this.next = null;
+    }
+  }
+  
+  class SinglyLinkedList{
+  
+    constructor(){
+      this.head = null;
+      this.tail = null;
+      this.length = 0;
+    }
+  
+    push(val){
+      var newNode = new Node(val);
+      if(!this.head){
+        this.head = newNode;
+        this.tail = this.head;
+      }else {
+        this.tail.next = newNode;
+        this.tail = newNode;
+      }
+      this.length++;
+      return this;
+    }
+  
+  }
+
+
+  var obj1 = new SinglyLinkedList()
+  obj1.push("First")
+//   obj1.push("Second")
+//   obj1.push("Third")
+
+  var obj = obj1.push()
+  console.log(obj)
