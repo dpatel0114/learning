26098af620b31ba769b6b87013acdc12ca772ed5
dpatel0114 <1991dharapatel@gmail.com>
2019-10-22 11:41:33 -0400

    queue

--- Queue.js
+++ Queue.js
@@ -17,4 +17,21 @@ class Queue {
         this.size = 0;
     }
 
+    enqueue(val){
+        var newNode = new Node(val);
+        if(!this.first){
+            this.first = newNode;
+            this.last = newNode;
+        }else{
+            this.last.next = newNode;
+            this.last = newNode;
+        }
+        return ++this.size; 
+    }
+
+    dequeue(){
+
+
+    }
+
 }
