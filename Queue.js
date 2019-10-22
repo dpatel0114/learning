fa07c5cbfbf144fd500b0fc4cc3af39a20c0f12d
dpatel0114 <1991dharapatel@gmail.com>
2019-10-22 11:51:16 -0400

    queue method

--- Queue.js
+++ Queue.js
@@ -30,8 +30,15 @@ class Queue {
     }
 
     dequeue(){
+        if(!this.first) return null;
 
-
+        var temp = this.first;
+        if(this.first === this.last){
+            this.last = null;
+        }
+        this.first = this.first.next;
+        this.size--;
+        return temp.value;
     }
 
 }
