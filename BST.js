a434470e62778e1a4236ad68e8b792c3ec4f21a2
dpatel0114 <1991dharapatel@gmail.com>
2019-10-28 12:53:28 -0400

    methods

--- BST.js
+++ BST.js
@@ -1,9 +1,21 @@
-class Node {
-    
+// ? DATA STRUCTURE: 4. BST
+// TODO: First INSERT method;
 
+class Node {
+    constructor(value){
+        this.value = value;
+        this.left = null;
+        this.right = null;
+    }
 }
 
 class BST{
+    constructor(){
+        this.root = null;
+    }
 
-
+    // insertion : add node to the tree -> will take value in as parameter;
+    insert(){
+        
+    }
 }
