5fc004aae918696d61eb7cdb9c58d976ce2c978d
dpatel0114 <1991dharapatel@gmail.com>
2019-10-30 10:49:23 -0400

    insert method

--- BST.js
+++ BST.js
@@ -15,7 +15,27 @@ class BST{
     }
 
     // insertion : add node to the tree -> will take value in as parameter;
-    insert(){
-        
+    insert(value){
+        var newNode = new Node(value);
+        if(this.root === null){
+            this.root = newNode;
+            return this;
+        }
+        var current = this.root;
+        while(true){
+            if(value === current.value) return undefined;
+            if(value < current.value){
+                if(current.left === null){
+                    current.left = newNode;
+                    return this;
+                }
+                current = current.left;
+            }
+            if(current.right === null){
+                current.right = newNode;
+                return this;
+            }
+            current = current.right;
+        }
     }
 }
