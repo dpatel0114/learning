0dd604bffa55309b104015bab0a40687b2b9871d
dpatel0114 <1991dharapatel@gmail.com>
2019-11-04 22:27:25 -0500

    find method

--- BST.js
+++ BST.js
@@ -39,5 +39,20 @@ class BST{
         }
     }
 
-   
+    find(value){
+        if(this.root === null) return false;
+        var current = this.root;
+            found = false; 
+          while(current && !found){
+            if(value < current.value){
+              current = current.left
+            }else if(value > current.value){
+              current = current.right
+            }else {
+              found = true;
+            }
+          }
+          if(!found) return undefined;
+          return current;
+      }
 }
