c011bac4b5bb6a5c454997c3022a4ff39fdb9ae1
dpatel0114 <1991dharapatel@gmail.com>
2019-11-06 21:10:49 -0500

    contains method

--- BST.js
+++ BST.js
@@ -55,4 +55,20 @@ class BST{
           if(!found) return undefined;
           return current;
       }
+
+      contains(value){
+        if(this.root === null) return false;
+        var current = this.root;
+            found = false; 
+          while(current && !found){
+            if(value < current.value){
+              current = current.left
+            }else if(value > current.value){
+              current = current.right
+            }else {
+              return true;
+            }
+          }
+          return false;
+      }
 }
