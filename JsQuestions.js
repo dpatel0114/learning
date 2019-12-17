dfe0550156b7d7910bcbeacf002c3f3ced25f965
dpatel0114 <1991dharapatel@gmail.com>
2019-12-17 14:43:41 -0500

    new

--- JsQuestions.js
+++ JsQuestions.js
@@ -1,7 +1,7 @@
-// Q: What is closure in jS? ==> frequantly asked que in interview 
-// A: When you define function and you do not passed parameter, still you can access that data within function from outer function or outer scope its called closure. 
-// closures:  are created whenever a variable tht is defined outside the current scope is accessed from within some inner                scope. 
-let addTo = function(passed){
+//* Q: What is closure in jS? ==> frequantly asked que in interview 
+    // A: When you define function and you do not passed parameter, still you can access that data within function from outer function or outer scope its called closure. 
+    // closures:  are created whenever a variable tht is defined outside the current scope is accessed from within some inner                scope. 
+    let addTo = function(passed){
 
     let add = function(inner){
         return passed + inner // In this function we did not pass parameter called passed still you can acess that from the outer function 
@@ -15,7 +15,8 @@ let addTo = function(passed){
     console.dir(tryMe)
     console.log(tryMe(2)) // tryMe is add function so when u call tryMe It will run add function and giving you the answer of add()
 
-// Q: How to create Private variable in Javascript?
+
+//* Q: How to create Private variable in Javascript?
     function secretVariable(){
         var private = xyz
     }
@@ -32,15 +33,15 @@ let addTo = function(passed){
         // ans: xyz 
 
     
-// Q: What is output?
-console.log(typeof(typeof 1))
-// A: typeof 1 ==> number 
-// typeof number ==> string 
-// typeof('number') ====> ans will be string 
+//* Q: What is output?
+    console.log(typeof(typeof 1))
+    // A: typeof 1 ==> number 
+    // typeof number ==> string 
+    // typeof('number') ====> ans will be string 
 
 
-// Q: how to add something to end or begining?
-var myArray = [a,b,c,d]
+//* Q: how to add something to end or begining?
+    var myArray = [a,b,c,d]
     myArry.push('end')
     myArray.unshift('start')
 
@@ -48,3 +49,9 @@ var myArray = [a,b,c,d]
     myArray = ['start', ...myArray]
     myArray = [...myArray, 'end']
     myArray= ['start', ...myArray, 'end']
+
+
+//* Q: What are the data types in javascript?      
+    // A: there are two datatype : primitive and object 
+   // ?  primitive: String, null, undefined, boolean, number, symbol
+    
